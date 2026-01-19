2626b5b6c5a54fa1509a5bc89bc2c29825b1fac9
Neil-05 <neilparkhe@gmail.com>
2026-01-19 11:00:23 +0530

    Q2

---
+++ C#/Q2-Multiplication_Table/Program.cs
@@ -0,0 +1,19 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {   
+        Console.WriteLine("Enter a number to print its multiplication table:");
+        int n=int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter a range up to which the table should be printed:");
+        int r=int.Parse(Console.ReadLine());
+        List<int> table = new List<int>();
+        for(int i = 0; i < r; i++)
+        {
+            table.Add(n * (i + 1));
+        }
+        Console.WriteLine("Multiplication Table -----");
+        Console.WriteLine("[" + string.Join(",", table) + "]");
+
+    }
+
+}
