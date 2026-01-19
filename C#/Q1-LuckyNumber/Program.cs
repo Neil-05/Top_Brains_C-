04bf0dc03df04862a5173a51fad5227f495a9fc9
Neil-05 <neilparkhe@gmail.com>
2026-01-19 11:01:57 +0530

    Name change

--- C#/Q1-LuckyNumber/Program.cs
+++ C#/Q1-LuckyNumber/Program.cs
@@ -1,9 +1,13 @@
 ﻿public class Program
 {
     public static void Main(string[] args)
-    {
+    {   Console.WriteLine("Enter a number:");
         int n=int.Parse(Console.ReadLine());
-        int temp=n;
+        Console.WriteLine("Enter Another number for range:");
+        int r=int.Parse(Console.ReadLine());
+        for(int i=n;i<=r;i++)
+        {
+        int temp=i;
         int sum=0;
         int count=0;
         while(temp>0)
@@ -13,7 +17,7 @@
             count++;
             temp=temp/10;
         }
-        int sq= n*n;
+        int sq= i*i;
         int sum2=0;
         while(sq>0)
         {
@@ -23,12 +27,10 @@
         }
         if(sum*sum == sum2)
         {
-            Console.WriteLine("This is a Lucky Number");
+            Console.WriteLine(i+" ,This is a Lucky Number");
         
         }
-        else
-        {
-            Console.WriteLine("This is not a Lucky Number");
-        }
+       
+    }
     }
 }
